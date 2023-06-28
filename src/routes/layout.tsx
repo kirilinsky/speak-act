b212514f5014b3938439b68b31d08f0357b6ab43
kirilinsky <kirill.ilinsky@gmail.com>
2023-06-28 11:27:40 +0200

    chore: cut remians

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,7 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
+ 
 import styles from './styles.css?inline';
 
 export const useServerTimeLoader = routeLoader$(() => {
@@ -16,11 +14,10 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
+    
       <main>
         <Slot />
-      </main>
-      <Footer />
+      </main> 
     </>
   );
 });
